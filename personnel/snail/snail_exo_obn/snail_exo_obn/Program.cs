307e6f7aa798b0534ba1036044b41e9c4fc59eb0
Borodkin Oleksandr Stepanovych <150446572+SachaBorodkin@users.noreply.github.com>
2025-08-29 11:08:51 +0200

    Added snail exercice

---
+++ personnel/snail/snail_exo_obn/snail_exo_obn/Program.cs
@@ -0,0 +1,24 @@
+﻿int snailHP = 50;
+
+int startPositionLeft = 0;
+int startPositionTop = 10;
+Console.SetCursorPosition(startPositionLeft, startPositionTop);
+Console.ForegroundColor = ConsoleColor.Blue;
+for (int i = snailHP; i >= 0; i--)
+{
+    Console.Clear();
+    
+   
+
+    startPositionLeft += 1;
+    Console.SetCursorPosition(startPositionLeft, startPositionTop);
+    Console.Write("_@_ö");
+    Thread.Sleep(200);
+    if (i == 0)
+    {
+        Console.Clear();
+        Console.SetCursorPosition(startPositionLeft, startPositionTop);
+        Console.Write("____");
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
