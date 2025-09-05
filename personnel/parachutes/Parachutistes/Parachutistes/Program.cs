dc5b4c6ccf478011856acc94a8bcf22ff2457cf7
Borodkin Oleksandr Stepanovych <150446572+SachaBorodkin@users.noreply.github.com>
2025-09-05 10:17:14 +0200

    feat(parachutiste) : ajout d'un avion qui vole

--- personnel/parachutes/Parachutistes/Parachutistes/Program.cs
+++ personnel/parachutes/Parachutistes/Parachutistes/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 static class Config
 {
     public const int SCREEN_HEIGHT = 40;
@@ -7,14 +8,64 @@ static class Config
 
 class Program
 {
+
     static void Main()
     {
-      
+
         Console.SetBufferSize(Config.SCREEN_WIDTH, Config.SCREEN_HEIGHT);
 
- 
+
         Console.SetWindowSize(Config.SCREEN_WIDTH, Config.SCREEN_HEIGHT);
 
-     
+        plane plane = new plane(0, 0); ;
+        while (true)
+        {
+            plane.draw();
+            plane.update();
+           
+        }
+ 
     }
+
+
+
+    class plane
+    {
+        private string[] view =
+            {
+            @" _                         ",
+            @"| \                        ",
+            @"|  \       ______          ",
+            @"--- \_____/  |_|_\____  |  ",
+            @"  \_______ --------- __>-} ",
+            @"        \_____|_____/   |  "
+        };
+
+        private int _startPositionX;
+        private int _startPositionY;
+        public plane(int startPositionX, int startPositionY)
+        {
+            this._startPositionX = startPositionX;
+            this._startPositionY = startPositionY;
+        }
+
+        public void draw()
+        {
+
+            
+            for (int i = 0; i < view.Length; i++) {
+                Console.SetCursorPosition(_startPositionX, i);
+                Console.Write(view[i]);
+                
+            }
+       
+            Thread.Sleep(100);
+            Console.Clear();
+        }
+        public void update()
+        {
+            _startPositionX += 1;
+        }
+    }
+
 }
