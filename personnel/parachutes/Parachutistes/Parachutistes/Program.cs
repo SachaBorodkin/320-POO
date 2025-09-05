bd66997ebca7ebf8c15ad1c3b1289c7478d6b63d
Borodkin Oleksandr Stepanovych <150446572+SachaBorodkin@users.noreply.github.com>
2025-09-05 09:09:20 +0200

    feat(parachutes) : ajout de la changement de la taille de la console

---
+++ personnel/parachutes/Parachutistes/Parachutistes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+static class Config
+{
+    public const int SCREEN_HEIGHT = 40;
+    public const int SCREEN_WIDTH = 150;
+}
+
+class Program
+{
+    static void Main()
+    {
+      
+        Console.SetBufferSize(Config.SCREEN_WIDTH, Config.SCREEN_HEIGHT);
+
+ 
+        Console.SetWindowSize(Config.SCREEN_WIDTH, Config.SCREEN_HEIGHT);
+
+     
+    }
+}
